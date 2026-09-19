ac2e1639bb59473ab585f974e2ad6ca9a30a7eca
starpaperlab <maribel.keena@gmail.com>
2026-09-19 10:58:13 -0400

    chore: remove superseded migration filename 20260919155300_block_a_resource_fk_indexes.sql

--- supabase/migrations/20260919155300_block_a_resource_fk_indexes.sql
+++
@@ -1,5 +0,0 @@
-begin;
-create index if not exists idx_business_expenses_user_id on public.business_expenses(user_id);
-create index if not exists idx_business_materials_user_id on public.business_materials(user_id);
-create index if not exists idx_business_equipment_user_id on public.business_equipment(user_id);
-commit;
